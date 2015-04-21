760f620f6091aab3af799402531927236fbee7ff
Stephen <steveyoung359@gmail.com>
2015-04-21 14:01:44 -0400

    updated gulp, and added auto zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,44 @@
 var gulp = require('gulp');
 var inlineCss = require('gulp-inline-css');
 var sass = require('gulp-sass');
- 
-gulp.task('build', function() {
-  return gulp.src('./*.html')
-    .pipe(inlineCss())
-    .pipe(gulp.dest('dist/'));
+var zip = require('gulp-zip');
+
+var paths = {
+  scss: './src/scss/**/*.scss',
+  html: './src/index.html',
+  assets: './src/images/**.*',
+  distDir: 'dist/',
+  distImages: 'dist/images',
+  cssDir: './src/css'
+};
+
+gulp.task('default', ['build', 'zip'], function() {
+  return gulp.watch([
+      paths.scss,
+      paths.html,
+      paths.assets
+    ], ['default']);
 });
 
-gulp.task('sass', function () {
-  gulp.src('./scss/*.scss')
+gulp.task('build', ['sass'], function() {
+  return gulp.src(paths.html)
+    .pipe(inlineCss())
+    .pipe(gulp.dest(paths.distDir));
+});
+
+gulp.task('sass', ['img-copy'], function () {
+  gulp.src(paths.scss)
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest(paths.cssDir));
+});
+
+gulp.task('img-copy', function() {
+  return gulp.src(paths.assets)
+  .pipe(gulp.dest(paths.distImages));
+});
+
+gulp.task('zip', ['build'], function () {
+  return gulp.src('src/*')
+    .pipe(zip('archive.zip'))
+    .pipe(gulp.dest('dist'));
 });
